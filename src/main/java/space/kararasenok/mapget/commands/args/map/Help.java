e7db7a876360c272bb983f163e75a76844878874
Kararasenok =) <95347545+kararasenok-gd@users.noreply.github.com>
2026-05-10 03:19:27 +0300

    refactor: removed /map help (temporary)

--- src/main/java/space/kararasenok/mapget/commands/args/map/Help.java
+++ src/main/java/space/kararasenok/mapget/commands/args/map/Help.java
@@ -20,6 +20,7 @@ public class Help implements Argument {
 
     @Override
     public void handler(CommandSender sender, String[] args) {
-        sender.sendMessage(MiniMessage.deserialize("===== <b><aqua><hover:show_text:'haii :3'>MapGet help</hover></aqua></b> =====\n<dark_gray><i>You can click on commands btw</i></dark_gray>\n\n<click:suggest_command:'/map help'><gold>/map help</gold></click> - Show that message\n<click:suggest_command:'/map get '><gold>/map get <url> <crop (true/<i>false</i>)></gold></click> - Get map with your image. I recommend to use <click:open_url:'https://catbox.moe/'><aqua><hover:show_text:'Also don\\'t forget to register an account there. Just in case :3'>catbox.moe</hover></click>.\n<click:suggest_command:'/map reload'><gold>/map reload</gold> - Reload config file</click>"));
+        sender.sendMessage(MiniMessage.deserialize("""
+        Theres nothing for now"""));
     }
 }
