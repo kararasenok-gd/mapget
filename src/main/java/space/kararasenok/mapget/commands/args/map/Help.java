8ce451b5d8a891e89d6b2f077705b8225d520fb0
Kararasenok =) <95347545+kararasenok-gd@users.noreply.github.com>
2026-05-10 18:52:53 +0300

    feat: new /map help

--- src/main/java/space/kararasenok/mapget/commands/args/map/Help.java
+++ src/main/java/space/kararasenok/mapget/commands/args/map/Help.java
@@ -6,6 +6,8 @@ import space.kararasenok.mapget.commands.Argument;
 import space.kararasenok.mapget.commands.handlers.MapHandler;
 import space.kararasenok.mapget.utils.MiniMessage;
 
+import java.util.Map;
+
 public class Help implements Argument {
     public static final String NAME = "help";
     public static final String DESC = "Show information about the commands";
@@ -28,6 +30,33 @@ public class Help implements Argument {
 
     @Override
     public void handler(CommandSender sender, String[] args) {
-        sender.sendMessage(MiniMessage.deserialize("Theres nothing for now. Maybe later I move it to wiki or idk"));
+        Map<String, Argument> argmap = mapHandler.getArgumentMap();
+
+        if (args.length == 1) {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("===== <b><aqua><hover:show_text:'haii :3'>MapGet help</hover></aqua></b> =====\n");
+            sb.append("<gray><i>Click on command to view detailed info</i></gray>\n\n");
+
+            for (Map.Entry<String, Argument> entry : argmap.entrySet()) {
+                String name = entry.getKey().toLowerCase();
+                Argument arg = entry.getValue();
+
+                sb.append(String.format("<click:run_command:'/map help %s'><gold>/map %s</gold></click> - %s\n", name, name, arg.description(false)));
+            }
+
+            sender.sendMessage(MiniMessage.deserialize(sb.toString()));
+        } else {
+            String name = args[1];
+            Argument cmdArg = argmap.get(name);
+
+            StringBuilder sb = new StringBuilder();
+            sb.append(String.format("===== <b><aqua><hover:show_text:'haii :3'>MapGet help - /map %s</hover></aqua></b> =====\n", name));
+            sb.append(cmdArg.description(true));
+
+            if (!sb.toString().endsWith("\n")) { sb.append("\n"); }
+
+            sender.sendMessage(MiniMessage.deserialize(sb.toString()));
+        }
     }
 }
